8f263d614d69c64a7390ef6ef8c6bcac2a4d58c9
toolbook <toolbook@163.com>
2020-08-30 12:31:57 +0800

    update:优化代码

--- src/main/java/com/tq/code/TqLeetcodeApplication.java
+++ src/main/java/com/tq/code/TqLeetcodeApplication.java
@@ -3,6 +3,9 @@ package com.tq.code;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ */
 @SpringBootApplication
 public class TqLeetcodeApplication {
 
